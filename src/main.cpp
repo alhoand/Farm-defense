1aaa9eb8ae057fe38c018f5107af5a2bf40a6c37
Zwenkka <henry.pietila@aalto.fi>
2020-07-20 18:39:34 +0300

    basic test in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,17 @@
 #include "tower.hpp"
 #include "enemy.hpp"
 #include "bullet.hpp"
+#include "test_tower.hpp"
+#include "position.hpp"
  
-int main(int argc, char *argv[]){
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+int main(){
+	Position pos = Position(1,2);   
+    std::cout << pos << std::endl;
+   
+	TestTower testi = TestTower(Position(1,2));
+	
+	std::cout << "Testing tower" << std::endl;
+	testi.Shoot();
+	testi.Update();
+	std::cout << "Testing done" << std::endl;
 }
