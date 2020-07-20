8e7261c2d3d9f95c971cedcb4a8663ae4a415799
Zwenkka <henry.pietila@aalto.fi>
2020-07-20 18:05:44 +0300

    added constructor and operator overload <<

--- src/position.hpp
+++ src/position.hpp
@@ -5,10 +5,20 @@
 
 class Position {
     public:
+
+		Position(int x, int y): x_(x), y_(y) {};
+		
+		
         //getter function for coordinates
         std::pair<int, int> GetPosition() {
             return std::make_pair(x_, y_);
         }
+		
+		friend std::ostream& operator<<(std::ostream& os, const Position p){
+			os << "[" << p.x_ << " , " << p.y_ << "]" << std::endl;
+			return os;
+		}
+		
 
     private:
         int x_;
