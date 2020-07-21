5d972994d0736339b1929fbd35a2f28a78b9139a
Jonna Mikkonen <jonna.mikkonen@aalto.fi>
2020-07-21 16:31:01 +0300

    add towers folder

---
+++ src/towers/test_tower.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../tower.hpp"
+
+#include <iostream>
+#include <string>
+
+class TestTower: public Tower{
+	public:
+		TestTower(Position p): Tower(p) {}
+		
+		void Update(){
+				std::cout << "TestTower updated" << std::endl;
+		}
+		
+		void Shoot(){
+				std::cout << "TestTower shot" << std::endl;
+		}
+};
