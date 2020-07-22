035773769c7ee388d7d86331cc065a8dec5f29b8
Anders Alho <anders.alho@aalto.fi>
2020-07-22 15:18:17 +0300

    Blob on the screen with SFML

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "position.hpp"
 #include "towers/test_tower.hpp"
 #include "enemies/test_enemy.hpp"
+#include <SFML/Graphics.hpp>
  
 int main(){
 	Position pos = Position(1,2);   
@@ -27,5 +28,50 @@ int main(){
     std::cout << *p1 << std::endl;
     TestEnemy* enemy1 = new TestEnemy(path1);
 
+    
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
+
+
+/*
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}*/
