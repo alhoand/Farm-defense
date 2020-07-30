798d9ff35b0b2c4e3a132f7da97e745810433a83
Mimi Mokka <mimi.mokka@aalto.fi>
2020-07-30 18:17:23 +0300

    Rough draft for Tower class and its implementation

--- src/tower.cpp
+++ src/tower.cpp
@@ -1,1 +1,37 @@
 //TODO class implementation here
+#include <tower.hpp>
+
+// Associates enemies with the corresponding textures
+// Textures are images that live on the graphics card
+
+Textures::ID toTextureID(Tower::Type type) {
+    switch (type) {
+    case Tower::Type::Fire:
+        return Textures::ID::Fire;
+    case Tower::Type::Water:
+        return Textures::ID::Water;
+    case Tower::Type::Leaf:
+        return Textures::ID::Leaf;
+    default: 
+        return Textures::ID::Fire;
+    }
+}
+// Constructor that works with SFML
+Tower::Tower(Type type, const TextureHolder &textures, Position position, int range, int reload_speed, Bullet bullet)
+         : type_(type), sprite_(textures.Get(toTextureID(type))), position_(position), range_(range),
+            reload_speed_(reload_speed), bullet_(bullet) { 
+        sf::FloatRect bounds = sprite_.getLocalBounds();
+        sprite_.setOrigin(bounds.width/2.f, bounds.height/2.f);
+}
+
+// Default constructor with hard-coded values for hitpoints for testing
+// Tower::Tower() : type_(Tower::Type::Fire) { }
+
+
+void Tower::DrawCurrent(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(sprite_, states);
+}
+
+void Tower::UpdateCurrent(sf::Time dt) {
+    // what's the logic here?
+}
--- src/tower.hpp
+++ src/tower.hpp
@@ -3,18 +3,36 @@
 //Initial header file for abstract tower class
 #include "position.hpp"
 #include "bullet.hpp"
+#include "entity.hpp"
+#include "resource_identifiers.hpp"
 
 /*  ***TODO***
 * - all return values are void, to be changed for right ones
 */
 
-class Tower {
+class Tower : public Entity {
     public:
-        //Constructor
-        Tower(Position p) : position_(p) {};
+        enum class Type {
+            Fire,
+            Water,
+            Leaf
+        };
+
+        //Constructors
+
+        // Default constructor - is it needed?
+        Tower();
+
+        // Constructor which is given the position
+        // Tower(Position p);
+        
+        // The constructor for now. Most of the parameters should be given automatically
+        Tower(Type type, const TextureHolder &textures, Position position, int range, int reload_speed, Bullet bullet);
 
         //Destructor
-        virtual ~Tower() {};
+        virtual ~Tower();
+
+        virtual void DrawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
 
         //Update the state of the tower, should be virtual
         virtual void Update() = 0;
@@ -23,9 +41,13 @@ class Tower {
         virtual void Shoot() = 0;
 
     protected:
+        virtual void UpdateCurrent(sf::Time dt);
+
+        Type type_;
+        sf::Sprite sprite_;
         int range_;
         Position position_;
         //enemies_in_range_;
         int reload_speed_;
-        //Bullet bullet_type_;
+        Bullet bullet_;
 };
