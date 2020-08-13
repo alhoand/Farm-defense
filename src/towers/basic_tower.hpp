aa3320d7359357dc89c54d172f55dd3201f699d3
Mimi Mokka <mimi.mokka@aalto.fi>
2020-08-13 12:45:16 +0300

    New inherited Tower class

---
+++ src/towers/basic_tower.hpp
@@ -0,0 +1,16 @@
+#include "tower.hpp"
+
+/* This is a basic tower that
+ * - shoots basic bullets which cost one hp for the enemy
+ * - is moderately fast
+ * - has a medium range
+*/
+
+class BasicTower : public Tower {
+    public:
+        // Constructor
+        BasicTower(const TextureHolder &textures);
+
+
+    private:
+}
